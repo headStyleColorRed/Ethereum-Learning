98d7516b4584089ed3987cc1a3612d946d4fede7
Rodrigo Labrador <headstylecolorred@gmail.com>
2021-02-13 14:35:29 +0100

    not really working..

--- solidity/main.js
+++ solidity/main.js
@@ -1,15 +1,21 @@
 const Web3 = require("web3")
 const web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:7545"))
-const solc = require("solc")
 const fs = require("fs")
 
 
 // List of accounts
-console.log(web3.eth.accounts);
 
 // Creating and compiling smart contract
-let smartContract = fs.readFileSync("./greetings.sol").toString()
-let compiledContract = solc.compile(smartContract)
-console.log(compiledContract);
+let compiledContract = require("./build/Greetings.json")
+let contractABI = compiledContract.abi
+let uploadContract = new web3.eth.Contract(contractABI)
+let byteCode = compiledContract.bytecode
+let deployedContract = uploadContract.deploy({
+    data: byteCode,
+    from: web3.eth.accounts.coinbase,
+    gas: 47000000
+})
+console.log(deployedContract);
+
 
 console.log("Done")
