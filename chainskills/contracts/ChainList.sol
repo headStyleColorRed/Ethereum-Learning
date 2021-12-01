508a338af08edefa00bf809ee7de6913ffe462d3
headStyleColorRed <headStyleColorRed@gmail.com>
2021-12-01 21:33:00 +0100

    added event to contract and deploy script

--- chainskills/contracts/ChainList.sol
+++ chainskills/contracts/ChainList.sol
@@ -8,11 +8,17 @@ contract ChainList {
     string description;
     uint256 price;
 
+    // Events
+    event LogSellArticle(address indexed _seller, string name, uint256 price);
+
+    // Methods
     function sellArticle(string memory _name, string memory _description, uint256 _price) public {
       seller = msg.sender;
       name = _name;
       description = _description;
       price = _price;
+
+      emit LogSellArticle(seller, name, price);
     }
 
     function getArticle() public view returns (address _seller, string memory _name, string memory _description, uint256 _price) {
